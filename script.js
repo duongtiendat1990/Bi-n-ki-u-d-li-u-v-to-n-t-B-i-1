22293e30255df5dcc85c873c21ff7a489e5b5dc1
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-03-27 11:21:59 +0700

    Bài 1

---
+++ script.js
@@ -0,0 +1,10 @@
+let Physics = parseInt(prompt("Input Physics Grade ",));
+let Chemistry = parseInt(prompt("Input Chemistry Grade ",));
+let Biology = parseInt(prompt("Input Biology Grade ",));
+let Sum = Physics + Chemistry + Biology
+let Average = (Physics + Chemistry + Biology)/3
+document.write("Physics Grade is " + Physics); document.write('</br>');
+document.write("Chemistry Grade is " + Chemistry); document.write('</br>');
+document.write("Biology Grade is " + Biology); document.write('</br>');
+document.write("Sum Grade is " + Sum); document.write('</br>');
+document.write("Average Grade is " + Average); document.write('</br>');
